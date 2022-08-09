d58edd9849bbc48f9932a9afca67ceff4950dbc1
Ömer Furkan İşleyen <54645034+OmerFI@users.noreply.github.com>
2022-08-10 02:15:26 +0300

    Use sentry

--- web/app.py
+++ web/app.py
@@ -3,6 +3,20 @@ from flask_restful import Api
 
 from resources import Subdomains, Whois
 
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
+
+sentry_sdk.init(
+    dsn="https://8e6b6d0c6cc441d5970d38c5e52af2ad@o1306235.ingest.sentry.io/6636657",
+    integrations=[
+        FlaskIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0
+)
 
 app = Flask(__name__)
 api = Api(app)
